999dcef1fc2fb2987577f87d4ed74a89c6cc7366
Vivek Sekhar <vivek@viveksekhar.ca>
2021-11-11 22:02:35 -0500

    Fetch hostname from cloud metadata

--- examples/hello/main.go
+++ examples/hello/main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"cloud.google.com/go/compute/metadata"
 )
 
 var port = flag.Uint("port", 0, "http listen port")
@@ -20,23 +22,36 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Path: %q\n", html.EscapeString(r.URL.Path))
+
+		// On GCP, OS provides hostnames like: "3b1179335f6d"
 		hostname, err := os.Hostname()
 		if err != nil {
 			fmt.Fprintf(w, "Error: %s\n", err)
 			w.WriteHeader(500)
 		}
-		fmt.Fprintf(w, "Host: %s\n", hostname)
+		fmt.Fprintf(w, "Hostname (OS): %s\n", hostname)
+
+		// On GCP, metadata service provides hostnames like:
+		//   "t8s-external-test-ctv7.northamerica-northeast2-b.c.fabula-8589.internal"
+		gcpHostname, err := metadata.Hostname()
+		if err != nil {
+			fmt.Fprintf(w, "Error: %s\n", err)
+			w.WriteHeader(500)
+		}
+		fmt.Fprintf(w, "Hostname (Cloud): %s\n", gcpHostname)
+
 		fmt.Fprintf(w, "Message: %s\n", *msg)
 		fmt.Fprintf(w, "Time: %d\n", time.Now().UnixNano())
 		log.Printf("request: %q\n", html.EscapeString(r.URL.Path))
 
 		if *downstream != "" {
+			fmt.Fprintf(w, "Downstream: %s\n", *downstream)
+			fmt.Fprintf(w, "Downstream response:\n")
 			resp, err := http.Get(*downstream)
 			if err != nil {
 				log.Printf("error getting downstream: %s", err)
 				w.WriteHeader(500)
 			}
-			fmt.Fprintf(w, "Downstream:\n")
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
 				log.Printf("error reading body: %s", err)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vsekhar/terranetes
 
 go 1.16
+
+require cloud.google.com/go v0.97.0
